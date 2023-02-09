82fb4d834b50325a9a917a55cca57f13436cdbc2
Levi Cole <levi.cole@roq.local>
2023-02-09 10:37:13 +0000

    Commit 2 - Implemented index js for add and view

---
+++ index.js
@@ -0,0 +1,35 @@
+const prompt = require('prompt-sync')();
+
+let rawInput;
+let formattedInput;
+
+const userList = []
+
+while(formattedInput != 'EXIT') {
+    rawInput = prompt('Enter a command: ');
+    formattedInput = rawInput.toUpperCase();
+
+    if (formattedInput == 'ADD') {
+
+        console.log('You have selected to add a user to the list');
+
+        if (userList.length == 0) {
+            console.log('The list is currently empty')
+        } else if (userList.length > 0) {
+            console.log(`The list is as follows ${userList}`);
+        }
+
+        const newUser = prompt("Please enter a new user: ");
+
+        userList.push(newUser);
+
+
+    } else if (formattedInput == 'VIEW') {
+        console.log('You have selected to view the list of users')
+        
+        for(let i = 0; i < userList.length; i++) {
+            console.log(`User ${i+1}: ${userList[i]}`);
+        }
+
+    }
+}
